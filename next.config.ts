99685aad068cc7622aa31069648a8bbd41b21b37
VARDHAN <vardhan@VALLURs-MacBook-Air.local>
2026-05-28 15:55:07 +0530

    fix: update next.config.ts properties for next.js 15

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@prisma/client', 'bcrypt'],
-    outputFileTracingIncludes: {
-      "/**/*": ["./prisma/**/*"],
-    },
+  serverExternalPackages: ['@prisma/client', 'bcrypt'],
+  outputFileTracingIncludes: {
+    "/**/*": ["./prisma/**/*"],
   },
 };
 
